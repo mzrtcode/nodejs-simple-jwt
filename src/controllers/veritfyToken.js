43f2f2ca14f54c0e8e85ee64b2bfbf1f3ec386fc
Stiven Medina <71569136+silenzrck@users.noreply.github.com>
2023-02-07 16:52:55 -0500

    Updating middleware verifyToken

--- src/controllers/veritfyToken.js
+++ src/controllers/veritfyToken.js
@@ -3,17 +3,25 @@ import config from '../config.js'
 
 
 function verifyToken(req,res, next){
-    const token = req.headers['x-access-token'];
-     if(!token){
+    /* const flag = true;
+    if(!flag) return res.json({message: 'invalid token'})
+    next() */
+    
+    const token = req.headers['x-access-token'] || req.query.token;
+    if(!token){
         return res.status(401).json({
             auth: false,
             message: 'No token provided'
         })
-     }
-     const decoded = jwt.verify(token, config.secret);
-     req.userId = decoded.id;
+    }
     
-     next();
+    jwt.verify(token,secretJWT,(err,decoded)=>{
+        
+        if(err) return res.status(404).json({message: 'Invalid token or expired'})
+        req.user = decoded
+        next()
+
+    })
 }
 
 export default verifyToken;
